ef7cd974657f03e75f2e7e4e19d6ddae1d09e11c
CJBiohacker <cj.moyses@gmail.com>
2021-07-06 18:07:58 -0300

    'Métodos de vincular o JavaScript em HTML'

---
+++ Aula-JavaScript/Introdução-JS/main.js
@@ -0,0 +1,16 @@
+// Comando que gera uma janela de notificação toda vez que a página é carregada ou recarregada.
+alert("Meu 1º JavaScript dentro do HTML(external).")
+
+// Declaração de variáveis.
+var nome = `Carlos Junior`;         // Variável de escopo global com valor atribuído à uma 'string'.
+let idade = 31;                     // Variável de escopo local com valor atribuído à um 'number integer'.
+const ideo = `Altermundialismo`;    // Constante de escopo global com valor atribuído à uma 'string'.
+
+console.log(`Nome: ${nome}
+Idade: ${idade}
+Ideologia Política: ${ideo}`);
+
+var questao = `Existe fim para a realidade?`;
+
+console.log(questao);
+console.log(questao.replace("realidade", "fantasia"))
