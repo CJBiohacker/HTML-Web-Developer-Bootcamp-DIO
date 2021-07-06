d793147c22cd4a08469bfc308849d37f9e0b554a
CJBiohacker <cj.moyses@gmail.com>
2021-07-06 19:13:51 -0300

    'Arrays no JavaScript'

--- Aula-JavaScript/Introdução-JS/main.js
+++ Aula-JavaScript/Introdução-JS/main.js
@@ -9,8 +9,45 @@ const ideo = `Altermundialismo`;    // Constante de escopo global com valor atri
 console.log(`Nome: ${nome}
 Idade: ${idade}
 Ideologia Política: ${ideo}`);
-
 var questao = `Existe fim para a realidade?`;
 
-console.log(questao);
-console.log(questao.replace("realidade", "fantasia"))
+console.log(questao);                                   // Comando que 'imprime' um conteúdo dentro do console do navegador.
+console.log(questao.replace("realidade", "fantasia"));  // 'Imprime' no console a variável 'questao' com o método de 'replace' que substitui a string 'realidade' por 'fantasia'.
+alert(questao.replace("a realidade", "o universo"));    // 'Imprime' no console a variável 'questao' com o método de 'replace' que substitui a string a 'a realidade' por 'o universo'.
+
+console.log(`Nome: ${nome}
+Idade: ${idade}
+Ideologia Política: ${ideo}`.toUpperCase());            // 'Imprime' no console a frase em strings e as variáveis com o método 'toUpperCase' que formata todas strings em letra maiúscula.
+
+console.log(questao.replace("realidade", "fantasia").toUpperCase());  // 'Imprime' no console a variável 'questao' com o método de 'replace' que substitui a string 'realidade' por 'fantasia', e depois com o método 'toUpperCase' formata todas as strings em letra maiúscula.
+
+// Situação em que ocorre o 'Hoisting' ou 'Içamento' do JavaScript.
+console.log(anoAtual - anoNasc);    // 'Imprime' no console a subtração entre as duas variáveis. Como eles foram declaradas após esta linha, o sistema classifica como 'NaN' ou 'Not-a-Number'.
+console.log(anoPandemia);           // 'Imprime' no console a variável 'anoPandemia'. Como ela foi declarado após esta linha de código, o sistema classifica como 'undefined'.
+
+var anoPandemia = '2020';
+var anoNasc = 1990;
+var anoAtual = 2021;
+
+console.log(`Ano em que iniciou a Pandemia: ${anoPandemia}.`);
+console.log(anoAtual - anoNasc);
+
+// Arrays
+// Criando uma Array
+var listaComida = ['Pizza', ' Kebab', ' Lasanha', ' Strogonoff', ' Risoto', ' Salmão'];
+
+console.log(`Lista de Comida: ${listaComida}`);
+console.log(`Quarto item da lista: ${listaComida[3]}`);
+
+listaComida.push(' Hot Filadelfia');                            // Adiciona um elemento no final da Array. Neste caso adicionou o Hot Filadelfia.
+console.log(`Lista de Comida (atualizada): ${listaComida}`);
+console.log(`Nº de elementos: ${listaComida.length}`);          // Imprime no console a extensão da Array.
+
+listaComida.pop();                                              // Remove um elemento do final da Array. Neste caso removeu o Hot Filadelfia.
+console.log(`Lista de Comida (atualizada): ${listaComida}`);
+
+listaComida.shift();                                            // Remove um elemento do início da Array. Neste caso removeu a Pizza.
+console.log(`Lista de Comida (atualizada): ${listaComida}`);
+
+
+
