416ce3a67e83e3a69793d739934c88d52172ea86
CJBiohacker <cj.moyses@gmail.com>
2021-07-06 21:46:49 -0300

    'Estrutura Condicional, Laços de Repetição e Date'

---
+++ Aula-JavaScript/Introdução-JS/cond-reps-date.js
@@ -0,0 +1,18 @@
+// Estrutura de Condicional (if...else)
+
+// var idade = prompt(`Qual sua idade ?`);                 // O comando prompt é um método DOM que retorna uma janela com um espaço de preenchimento ao carregar/recarregar a página HTML.
+
+// if (idade >= 18) {
+//     alert(`Desculpe o transtorno.`);
+//     console.log(`Desculpe o transtorno.`)
+// } else {
+//     console.log("VAI PRA CASA, MOLEQUE !!!")
+//     alert("VAI PRA CASA, MOLEQUE !!!")
+// }
+
+var count = 0;
+
+while (count < 5) {
+    console.log(count);
+    count = count++
+};
