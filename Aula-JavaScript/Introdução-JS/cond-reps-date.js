4e8ad06fa50518a717f3e4452737c23e85c28859
CJBiohacker <cj.moyses@gmail.com>
2021-07-06 22:32:05 -0300

    'Date no JavaScript'

--- Aula-JavaScript/Introdução-JS/cond-reps-date.js
+++ Aula-JavaScript/Introdução-JS/cond-reps-date.js
@@ -10,11 +10,12 @@
 //     alert("VAI PRA CASA, MOLEQUE !!!")
 // }
 
-// Laços de Repetição (while...) (do...while) (for...)
+// Laços de Repetição (while...) (do...while) (for)
 
 var count = 0;
 
 // Laço de repetição 'while'. Enquanto o 'count' for menor do que 5, imprime no console o valor atual do 'count' e soma +1 ao 'count' atual, repetindo o loop.
+console.log("Laço de Repetição while")
 while (count < 5) {
     console.log(count)
     count++
@@ -23,7 +24,31 @@ while (count < 5) {
 count = 0;  // Atribui o valor 0 ao 'count'.                                             
 
 // Laço de repetição 'do...while'. Imprime no console o valor atual do 'count' e soma +1 ao 'count' atual. Enquanto o 'count' for menor do que 5 repete o loop. 
+console.log("Laço de Repetição do...while")
 do {
     console.log(count)
     count++
 } while (count < 5);
+
+// Laço de repetição 'for'. Para, atribui o valor atual de 'count' para 0, sendo 'count' atual menor do que 5. Imprime no console o valor atual de 'count' e depois soma +1, repetindo o loop se for menor que 5.
+console.log("Laço de Repetição for")
+for (count = 0; count < 5; count++) {
+    console.log(count)
+}
+
+// Datas no JavaScript ( new Date(); )
+
+var data = new Date();
+
+console.log(`Dia do mês: ${data.getDate()}`);
+console.log(`Dia da semana: ${data.getDay() + 1}ª`);
+console.log(`Ano atual: ${data.getFullYear()}`);
+console.log(`Hora(s) atual(s): ${data.getHours()}h`);
+console.log(`Milissegundos do momento: ${data.getMilliseconds()}ms`);
+console.log(`Minuto(s) atual: ${data.getMinutes()}min`);
+console.log(`Mês atual: ${data.getMonth() + 1}`);
+console.log(`Segundos(s) atual: ${data.getSeconds()}s`);
+console.log(`Horário(ms) atual: ${data.getTime()}ms`);
+
+console.log(`Data e Horário atual: ${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()} ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`);
+
