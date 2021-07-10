c994934c759f7f4ba8fe5e9cbd397e08658d1bba
CJBiohacker <cj.moyses@gmail.com>
2021-07-10 01:49:13 -0300

    "Contagem de Cédulas"

--- Desafios JavaScript/Fundamentos Aritmeticos JavaScript.js
+++ Desafios JavaScript/Fundamentos Aritmeticos JavaScript.js
@@ -1,32 +1,42 @@
 let valor = 576;
 var x = valor;
 
-var notas100 = Math.floor(x / 100);      // Arredonda pro número inteiro inferior mais próximo.
-var a = (valor - (notas100 * 100));      // a = (valor - total de 'notas de 100' * 100).
-var notas50 = );
+// Math.floor ==> Arredonda pro número inteiro inferior mais próximo. Utilizado pra alcançar o número exato de notas de cada valor.
+var notas100 = Math.floor(x / 100);
+var a = (valor - (notas100 * 100));
+var notas50 = Math.floor(a / 50);
+var b = (a - (notas50 * 50));
+var notas20 = Math.floor(b / 20);
+var c = (b - (notas20 * 20));
+var notas10 = Math.floor(c / 10);
+var d = (c - (notas10 * 10));
+var notas5 = Math.floor(d / 5);
+var e = (d - (notas5 * 5));
+var notas2 = Math.floor(e / 2);
+var f = (e - (notas2 * 2));
+var notas1 = Math.floor(f);
 
 
-
-if (valor > 100) {  // 576 > 100
-    notas100
+if (valor >= 100) {                    // 576 >= 100 ==> TRUE
+    notas100                           // 576/100 = 5,76 ==> arredondando pra 5
 } else {
-    if (a > 50) {   //( 576 - 500 = 76) > 50  ==>  Neste caso é 76 > 56
-        notas50
+    if (a >= 50) {                     // 76 >= 50  ==> TRUE
+        notas50                        // 76/50 = 1,52 ==> arredondando pra 1
     } else {
-        if (    ) {   //
-            notas20
+        if (b >= 20) {                 // 26 >= 20 ==> TRUE
+            notas20                    // 26/20 = 2,6 ==> arredondando pra 2
         } else {
-            if (    ) {   //
-                notas10
+            if (c >= 10) {             // 6 >= 10 ==> FALSE
+                notas10                // 6/10 = 0,6 ==> arredondando pra 2
             } else {
-                if (    ) {   //
-                    notas5
+                if (d >= 5) {          // 6 >= 5 ==> TRUE
+                    notas5             // 6/5 = 1,2 ==> arredondando pra 1
                 } else {
-                    if (    ) {   //
-                        notas2
+                    if (e >= 2) {      // 1 >= 2 ==> FALSE
+                        notas2         //
                     } else {
-                        if (    ) {   //
-                            notas1
+                        if (f >= 1) {  // 1 >= 1 
+                            notas1     //
                         }
                     }
                 }
@@ -44,3 +54,11 @@ console.log(`${notas10} nota(s) de R$ 10,00`)
 console.log(`${notas5} nota(s) de R$ 5,00`)
 console.log(`${notas2} nota(s) de R$ 2,00`)
 console.log(`${notas1} nota(s) de R$ 1,00`)
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
